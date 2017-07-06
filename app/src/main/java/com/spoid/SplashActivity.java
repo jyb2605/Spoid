5012afdaeb528dc7039cc82cb0688210f78eae96
Hanul <nul010@naver.com>
2017-07-06 14:29:46 +0900

    Object Detection 이식 + Splash

--- app/src/main/java/com/spoid/SplashActivity.java
+++ app/src/main/java/com/spoid/SplashActivity.java
@@ -1,14 +1,53 @@
 package com.spoid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        handler = new Handler();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2500);
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Animation fade_out = AnimationUtils.loadAnimation(getApplicationContext(), android.R.anim.fade_out);
+                        }
+                    });
+
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                intent = new Intent(SplashActivity.this, CameraActivity.class);
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        startActivity(intent);
+                        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                        finish();
+                    }
+                });
+
+            }
+        }).start();
 
     }
+
+
 }

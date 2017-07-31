ea104094113215c6c6dd89c1bfbeda99159a235a
완빈 <a@ab.c>
2017-07-31 16:51:27 +0900

    create item class

---
+++ app/src/main/java/com/spoid/Item.java
@@ -0,0 +1,40 @@
+package com.spoid;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ * Created by user on 2017-07-31.
+ */
+
+public class Item {
+    String name;
+    String recipe;
+    String tool;
+    String definition;
+    String created_time;
+    int img_src;
+    int icon_src;
+
+    public Item() {
+    }
+
+    public Item(String name, String recipe, String tool, String definition,
+                int img_src, int icon_src) {
+        this.name = name;
+        this.recipe = recipe;
+        this.tool = tool;
+        this.definition = definition;
+        this.img_src = img_src;
+        this.icon_src = icon_src;
+        this.created_time = "";
+
+        created_time += Calendar.getInstance().get(Calendar.YEAR);
+        created_time+='.';
+        created_time += Calendar.getInstance().get(Calendar.MONTH);
+        created_time+='.';
+        created_time += Calendar.getInstance().get(Calendar.DATE);
+        
+    }
+}
